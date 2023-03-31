696b029a66808a61351e0ee8df0353d3b15cf4c8
Zharfan0 <zharfanwidyadana@gmail.com>
2023-03-31 14:59:45 +0700

    void input while

--- Quicksort/Quicksort.cpp
+++ Quicksort/Quicksort.cpp
@@ -6,3 +6,15 @@ int arr[20];            // Array of integers to hold values
 int cmp_count = 0;      // Number of comparison
 int mov_count = 0;      // Number of data movements
 int n;
+
+void input() {
+    while (true)
+    {
+        cout << "Masukkan panjang element array: ";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+        else
+            cout << "\nMaksimum panjang array adalah 20" << endl;
+    }
