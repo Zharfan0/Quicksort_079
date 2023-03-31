b6899047d38f2413c6537f7d31349bf294473742
Zharfan0 <zharfanwidyadana@gmail.com>
2023-03-31 15:05:57 +0700

    void display for

--- Quicksort/Quicksort.cpp
+++ Quicksort/Quicksort.cpp
@@ -96,3 +96,15 @@ void q_short(int low, int high)
     q_short(j + 1, high);                                       // Langkah Algoritma No.13
 
 }
+
+void display() {
+    cout << "\n-----------------" << endl;
+    cout << "Sorted Array" << endl;
+    cout << "-------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
